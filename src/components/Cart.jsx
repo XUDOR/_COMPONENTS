f556be67412538335db8ef64e3d86e5cbbf63cf3
Roderick Shoolbraid <rdxenv@gmail.com>
2024-02-05 20:01:35 -0800

    className added for css

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,12 +3,12 @@ import CartItem from "./CartItem";
 import Total from "./Total";
 
 const Cart = ({cartItems, onRemoveFromCart}) => (
-  <div>
-    <h2>CART</h2>
+  <div className="cart">
+    <h2 className="cart-title">CART</h2>
     {cartItems.map((item) => (  
       <CartItem key={item.id} item={item} onRemoveFromCart={onRemoveFromCart} />
     ))}
-    <Total cartItems={cartItems} />
+    < Total cartItems={cartItems} />
   </div>
 );
 
