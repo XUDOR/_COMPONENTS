12c6c122925289ad5a9d33a341b742be875c14de
Roderick Shoolbraid <rdxenv@gmail.com>
2024-02-05 17:11:08 -0800

    cart / cart item refactor / debug

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import Cart from './components/Cart';
 import './App.css';
 
 //Product Data
-const IntialProducts = [
+const InitialProducts = [
   {id: 1 , name: 'Product 1', price: 29.99 },
   {id: 2 , name: 'Product 2', price: 19.99 },
   {id: 3 , name: 'Product 3', price: 39.99 },
-  {id: 4 , name: 'Product 1', price: 9.99 },
+  {id: 4 , name: 'Product 4', price: 9.99 },
 ];
 
 
 function App() {
-  const [products] = useState(IntialProducts);
+  const [products] = useState(InitialProducts);
   const [cartItems, setCartItems] = useState([]);
 
   const handleAddToCart = (product) => {
@@ -26,8 +26,8 @@ function App() {
 
   return (
     <div className="App">
-      <ProductList products={products}onAddToCart={handleAddToCart}/>
-      <Cart cartItems={cartItems}onRemoveFromCart={handleRemoveFromCart}/>  
+      <ProductList products={products} onAddToCart={handleAddToCart} />
+      <Cart cartItems={cartItems} onRemoveFromCart={handleRemoveFromCart} />  
       
     </div>
   );
