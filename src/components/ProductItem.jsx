5b4f208898867fad3c08c7762c66f0b7a716153c
Roderick Shoolbraid <rdxenv@gmail.com>
2024-02-05 20:02:35 -0800

    className added for css

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const ProductItem = ({ product, onAddToCart }) => (
   <div className="product-tile">
     <h3 className="product-name">{product.name}</h3>
-    <p>${product.price}</p>
-    <button onClick={()=> onAddToCart(product)}>Add to Cart</button>
+    <p className="product-price">${product.price}</p>
+    <button className="add-product"onClick={()=> onAddToCart(product)}>+</button>
   </div>  
 );
 
