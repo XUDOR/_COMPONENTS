491b422033a17009b2fa5c85efb60775f4a7df93
Roderick Shoolbraid <rdxenv@gmail.com>
2024-02-05 20:57:08 -0800

    cart styling

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -4,7 +4,7 @@ const CartItem = ({ item, onRemoveFromCart }) => (
   <div className="cart-item">
     <span className="item-name">{item.name}</span>
     <span className="item-price">${item.price.toFixed(2)}</span>
-    <button className="remove-item" onClick={() => onRemoveFromCart(item.id)}>-</button>
+    <button className="remove-item" onClick={() => onRemoveFromCart(item.id)}>X</button>
   </div>
 );
 
