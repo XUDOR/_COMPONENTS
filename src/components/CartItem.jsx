620f99d57633049df1562411a6a229671820ff54
Roderick Shoolbraid <rdxenv@gmail.com>
2024-02-05 20:02:21 -0800

    className refactor

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const CartItem = ({ item, onRemoveFromCart }) => (
-  <div>
-    <span>{item.name}</span>
-    <span>   {item.price}</span>
-    <button onClick={() => onRemoveFromCart(item.id)}>-</button>
+  <div className="cart-item">
+    <span className="item-name">{item.name}</span>
+    <span className="item-price">${item.price.toFixed(2)}</span>
+    <button className="remove-item" onClick={() => onRemoveFromCart(item.id)}>-</button>
   </div>
 );
 
