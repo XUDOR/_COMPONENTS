18f218ac98ce7eac49d81514719fbc33dfcb6ede
Roderick Shoolbraid <rdxenv@gmail.com>
2024-02-08 15:07:42 -0800

    logic / math to cart UI

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const CartItem = ({ item, onRemoveFromCart }) => (
   <div className="cart-item">
-    <span className="item-name">{item.name}</span>
-    <span className="item-price">${item.price.toFixed(2)}</span>
+    <span className="item-name">{item.name} x {item.quantity}</span>
+    <span className="item-price">${(item.price * item.quantity).toFixed(2)}</span>
     <button className="remove-item" onClick={() => onRemoveFromCart(item.id)}>X</button>
   </div>
 );
