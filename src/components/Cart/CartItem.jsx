f06b538e1fc709c5710b7be67f6b9fc7a611eb09
Roderick Shoolbraid <rdxenv@gmail.com>
2024-02-09 11:58:26 -0800

    props for decrease

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const CartItem = ({ item, onRemoveFromCart }) => (
+const CartItem = ({ item, onRemoveFromCart, onDecreasedQuantity}) => (
   <div className="cart-item">
     <span className="item-name">{item.name} x {item.quantity}</span>
     <span className="item-price">${(item.price * item.quantity).toFixed(2)}</span>
