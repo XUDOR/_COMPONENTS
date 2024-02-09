99e45055e0959598e9ca1ca00c52ada21e5a49cb
Roderick Shoolbraid <rdxenv@gmail.com>
2024-02-09 11:59:21 -0800

    modification for decrease button

--- src/components/Cart/ProductList.jsx
+++ src/components/Cart/ProductList.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ProductItem from './ProductItem';
 
-const ProductList = ({ products, onAddToCart }) => (
+const ProductList = ({ products, onAddToCart, onDecreaseInCart}) => (
   <div className='product-list'>
     {products.map((product) => (
-      <ProductItem key={product.id} product={product} onAddToCart={onAddToCart} />
+      <ProductItem 
+        key={product.id} 
+        product={product} 
+        onAddToCart={onAddToCart} 
+        onDecreaseInCart={onDecreaseInCart} />
     ))}
   </div>
 );
