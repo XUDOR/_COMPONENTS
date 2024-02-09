c8cbe3901fd6ada691b8d9ea09a871f7f33475dd
Roderick Shoolbraid <rdxenv@gmail.com>
2024-02-09 11:58:49 -0800

    placement in div -onClick

--- src/components/Cart/ProductItem.jsx
+++ src/components/Cart/ProductItem.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const ProductItem = ({ product, onAddToCart }) => (
+const ProductItem = ({ product, onAddToCart, onDecreaseInCart}) => (
   <div className="product-tile">
     <h3 className="product-name">{product.name}</h3>
     <p className="product-price">${product.price}</p>
+    <button className="modify-product"onClick={()=> onDecreaseInCart(product)}>-</button>
     <button className="add-product"onClick={()=> onAddToCart(product)}>+</button>
   </div>  
 );
