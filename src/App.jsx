2c3de6d251eb384b70829d1c0c68c674ed15ece9
Roderick Shoolbraid <rdxenv@gmail.com>
2024-02-16 11:31:39 -0800

    basic styling

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Form from './components/Forms/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='titleBox'>
+        __Form
+      </div>
+      <div>
+        <Form/>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
--- src/components/Forms/form.jsx
+++ src/components/Forms/form.jsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-  const form = {
-  
+function Form() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+  });
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
   }
 
-export default form;
+  return (
+    <form onSubmit={handleSubmit}>
+      <div >
+        <label className="labelBox"  htmlFor="name">Name:</label>
+        <input className="nameBox"
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </div>
+        <label className="labelBox" htmlFor="email">Email:</label>
+        <input className="mailBox"
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          />
+      <br/>
+      <button className="submitBox" type="submit">Submit</button>
+      
+    </form>
+  );
+
+}
+
+export default Form;
